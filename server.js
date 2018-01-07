492fd1436233eaeb1d22a071e697fe66d44f43c4
Pedram-Nrz <Pedram_duan5@yahoo.com>
2018-01-07 14:04:01 +0330

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next)=>{
+    var nowDate = new Date().toDateString();
+    var nowTime = new Date().getHours().toString() + ":"+ new Date().getMinutes().toString()+":"+ new Date().getSeconds().toString();
+    if(fs.existsSync('log.txt')){
+        fs.appendFileSync('log.txt', "\n"+nowDate + "--"+ nowTime+" || " + req.url);
+    }else{
+        fs.writeFileSync('log.txt', "\n"+nowDate + "--"+ nowTime+" || " + req.url);
+    }
+    
+    next();
+    
+});
+
+//app.use((req, res, next)=>{
+//    res.render('megnets.hbs');
+////    next();
+//})
+app.use(express.static(__dirname+"/public"));
+
+hbs.registerHelper('getYear', () => {
+   return new Date().getFullYear()
+});
+
+hbs.registerHelper('capsMe', (toCap, pe) => {
+    toCap = hbs.Utils.escapeExpression(toCap);
+    return new hbs.SafeString(toCap.toUpperCase() + pe.toLowerCase());
+
+})
+
+app.get('/', (req, res) => {
+   res.render('home.hbs',{
+       welcomeMessage: 'happy to see you visiting my page',
+       pageTitle: 'Home Page'
+   });
+});
+
+app.get('/bad', (req, res) =>{
+    res.json({
+        errorMessage: 'Bad Gateway'
+    });
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+});
+
+app.listen(2000, () => {
+    console.log('Express started');
+});
